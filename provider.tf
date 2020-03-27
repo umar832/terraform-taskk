5d482dec1830bfd134753c5926d6b3f8d67317bd
umar832 <59777479+umar832@users.noreply.github.com>
2020-03-26 20:43:22 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+  alias  =  "virginia"
+  
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  alias  =  "oregon"
+
+}
+
+provider "aws"  {
+  region  = "us-east-2"
+  alias  =  "ohio"
+
+}
+
+
+provider "aws" {
+  region  = "us-west-1"
+  alias  =  "california"
+
+}
